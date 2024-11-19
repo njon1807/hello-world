6d22ac104be1c1c7d27692ae85bfde1ef7e2e46b
njon1807 <njon1807@students.vsu.edu>
2024-11-19 12:33:27 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "NjgtqpZCWT43",
+        "outputId": "5f1c407f-6414-4fef-85cf-fb9a2632d015"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Hello, World!\n"
+          ]
+        }
+      ],
+      "source": [
+        "# V00711807 Nicolas Jones\n",
+        "print(\"Hello, World!\")\n"
+      ]
+    }
+  ]
+}
